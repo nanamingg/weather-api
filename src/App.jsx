4efdb2e83a951800339c01773f0ed83ff78902d4
nanamingg <nana172@kakao.com>
2023-11-10 13:26:07 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { MdOutlineWbSunny } from "react-icons/md";
+
+const weatherIcon = {
+  "01": {
+    textColor: "text-red-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  "02": {
+    textColor: "text-orang-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  "03": {
+    textColor: "text-gray-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  "04": {
+    textColor: "text-gray-700",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  "09": {
+    textColor: "text-red-200",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  10: {
+    textColor: "text-blue-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  11: {
+    textColor: "text-cyan-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  13: {
+    textColor: "text-green-300",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+  50: {
+    textColor: "text-brown-500",
+    icon: <MdOutlineWbSunny size={120} />,
+  },
+};
 
 const App = () => {
   const [weatherData, setWeatherData] = useState();
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+
+  const getGeolocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  };
 
   const getWeather = async () => {
     const response = await axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?lat=37.5101776&lon=127.1220981&appid=c23ac157367173e0b9ea81cc4288a2a3"
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=f9cd232dbbb4ca1f4cf80eb8468af216&units=metric`
     );
 
     setWeatherData(response.data);
@@ -14,15 +63,28 @@ const App = () => {
   };
 
   useEffect(() => {
-    getWeather();
+    getGeolocation();
   }, []);
 
+  useEffect(() => {
+    if (!latitude || !longitude) return;
+
+    getWeather();
+  }, [latitude, longitude]);
+
   return (
-    <div className="bg-red-100">
+    <div className="min-h-screen flex flex-col justify-center items-center text-2xl">
       {weatherData ? (
-        <img
-          src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-        />
+        <div
+          className={`flex flex-col items-center gap-8 ${
+            weatherIcon[weatherData.weather[0].icon.substring(0, 2)].textColor
+          }`}
+        >
+          {weatherIcon[weatherData.weather[0].icon.substring(0, 2)].icon}
+          <div>
+            {weatherData.name}, {weatherData.main.temp}
+          </div>
+        </div>
       ) : (
         <div>로딩중...</div>
       )}
